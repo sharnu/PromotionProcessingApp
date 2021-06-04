3e6c83dabd8aa6eb4ec41217f444d2b756443dae
skonapure <skonapure@pelitas.com>
2021-06-04 10:12:31 +0530

    Installed Xunit and Implemented First Failing Test

---
+++ PromotionProcessingApp/PromotionEngineTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace PromotionProcessingApp
+{
+    public class PromotionEngineTest
+    {
+        private PromotionEngine _promotionEngine = new PromotionEngine();
+
+        [Fact]
+        public void ReturnsCartTotalForNonDiscountedProducts()
+        {
+            var products = GetProductsFromCart();
+
+            var result = _promotionEngine.CalculateCartTotal(products);
+
+            result.Should().Be(115m);
+        }
+
+        private List<Product> GetProductsFromCart()
+        {
+            return this.GetAllProducts();
+        }
+
+        private List<Product> GetAllProducts()
+        {
+            return new List<Product>()
+            {
+                new Product() { Id = 'A', Price = 50m},
+                new Product() { Id = 'B', Price = 30m},
+                new Product() { Id = 'C', Price = 20m},
+                new Product() { Id = 'D', Price = 15m},
+            };
+        }
+
+    }
+}
