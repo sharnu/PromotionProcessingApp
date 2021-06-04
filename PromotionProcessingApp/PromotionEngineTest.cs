8b22cff2fb0cf5973a6c6456e5a282b448fba109
skonapure <skonapure@pelitas.com>
2021-06-04 11:07:07 +0530

    New Test Scenario 2 Failed

--- PromotionProcessingApp/PromotionEngineTest.cs
+++ PromotionProcessingApp/PromotionEngineTest.cs
@@ -9,7 +9,7 @@ namespace PromotionProcessingApp.Tests
     {
         private PromotionEngine _promotionEngine = new PromotionEngine();
 
-        [Fact]
+        [Fact(DisplayName = "Scenario 1")]
         public void ReturnsCartTotalForNonDiscountedProducts()
         {
             var products = GetProductsFromCart();
@@ -17,6 +17,54 @@ namespace PromotionProcessingApp.Tests
             decimal result = _promotionEngine.CalculateCartTotal(products);
 
             result.Should().Be(115m);
+        }
+
+        [Fact]
+        public void ReturnsCartTotalForMultipleQuantitiesOfSameProduct()
+        {
+            var products = GetMultipleQuantitiesOfSameProductFromCart();
+
+            decimal result = _promotionEngine.CalculateCartTotal(products);
+
+            result.Should().Be(100m);
+        }
+
+        [Fact(DisplayName = "Scenario 2")]
+        public void ReturnsCartTotalForDiscountedProducts()
+        {
+            var products = GetProductsHavingDiscountFromCart();
+
+            decimal result = _promotionEngine.CalculateCartTotal(products);
+
+            result.Should().Be(320m);
+        }
+
+
+        private List<Product> GetProductsHavingDiscountFromCart()
+        {
+            return new List<Product>()
+            {
+                new Product('A', 50m),
+                new Product('A', 50m),
+                new Product('A', 50m),
+                new Product('A', 50m),
+                new Product('A', 50m),
+                new Product('B', 30m),
+                new Product('B', 30m),
+                new Product('B', 30m),
+                new Product('B', 30m),
+                new Product('B', 30m),
+                new Product('C', 20m),
+            };
+        }
+
+        private List<Product> GetMultipleQuantitiesOfSameProductFromCart()
+        {
+            return new List<Product>()
+            {
+                new Product('A', 50m),
+                new Product('A', 50m),
+            };
         }
 
         private List<Product> GetProductsFromCart()
