8bd2c8b1d3479d4b0b2747ffc5a4b6912e8a8569
skonapure <skonapure@pelitas.com>
2021-06-04 17:35:45 +0530

    Refactored Code - Moved Logic to individual Classes And All Test Pass

--- PromotionProcessingApp/PromotionEngine.cs
+++ PromotionProcessingApp/PromotionEngine.cs
@@ -11,6 +11,7 @@ namespace PromotionProcessingApp
     {
         Cart CalculateCartTotal(Cart cart);
     }
+
     public abstract class BasePromotion
     {
         #region Promotion InMemory Data Object
@@ -39,16 +40,23 @@ namespace PromotionProcessingApp
             if (cart != null && cart.CartItems.Any())
                 cart.Total = cart.CartItems.Sum(p => p.SubTotal);
         }
+
+        protected static Cart MapCart(Cart cart)
+        {
+            Cart result = new Cart();
+            result.CartItems = new List<CartItem>();
+            result.CartItems = cart.CartItems;
+            result.Id = cart.Id;
+            result.Total = cart.Total;
+            return result;
+        }
     }
 
     public class SingleProductFlatPricePromotion : BasePromotion, IPromotionRule
     {
-
         public Cart CalculateCartTotal(Cart cart)
         {
-            Cart result = new Cart();
-            result.CartItems = new List<CartItem>();
-            result.Id = cart.Id;
+            Cart result = MapCart(cart);
 
             if (cart != null && !cart.CartItems.Any())
                 return cart;
@@ -72,7 +80,8 @@ namespace PromotionProcessingApp
                             cartItemData.SubTotal = discountedQuantity * promotion.Value;
                             cartItemData.SubTotal += (quantity - (discountedQuantity * promotionQuantity)) * cartItemData.Product.Price;
                             cart.CartItems.First(c => c.Id == cartItemData.Id).IsPromotionApplied = true;
-                            result.CartItems.Add(cartItemData);
+                            //result.CartItems.Add(cartItemData);
+                            result.CartItems.Where(c=> c.Id == cartItemData.Id).Select(c=> { return cartItemData; });
                         }
                     }
                 }
@@ -82,56 +91,20 @@ namespace PromotionProcessingApp
 
             return result;
         }
-
     }
 
-    public class PromotionEngine
+    public class BundledProductsFlatPricePromotion : BasePromotion, IPromotionRule
     {
-        private IPromotionRepository promotionRepository;
-        private List<Promotion> _promotions = new List<Promotion>();
-
-        public PromotionEngine(IPromotionRepository promotionRepository)
-        {
-            this.promotionRepository = promotionRepository;
-            _promotions = promotionRepository.GetAllPromotions().ToList();
-        }
-
-
         public Cart CalculateCartTotal(Cart cart)
         {
-            Cart result = new Cart();
-            result.CartItems = new List<CartItem>();
-            result.Id = cart.Id;
+            Cart result = MapCart(cart);
 
             if (cart != null && !cart.CartItems.Any())
                 return cart;
 
-            SingleProductFlatPricePromotion singleProductFlatPricePromotion = new SingleProductFlatPricePromotion();
-
-           var productPromotion = _promotions.OrderBy(p => p.IsBundledPromotion);
-
+            var productPromotion = _promotions.OrderBy(p => p.IsBundledPromotion);
             foreach (var promotion in productPromotion)
             {
-                result = singleProductFlatPricePromotion.CalculateCartTotal(cart);
-                //if (promotion.IsBundledPromotion == false)
-                //{
-                //    var cartItemData = cart.CartItems
-                //                        .FirstOrDefault(c => c.Product.Id == promotion.Products.First().ProductId);
-
-                //    if (cartItemData != null)
-                //    {
-                //        int quantity = cartItemData.Quantity;
-                //        int promotionQuantity = promotion.Products.First().DiscountQuantity;
-                //        if (quantity >= promotionQuantity)
-                //        {
-                //            decimal discountedQuantity = decimal.Floor(quantity / promotionQuantity);
-                //            cartItemData.SubTotal = discountedQuantity * promotion.Value;
-                //            cartItemData.SubTotal += (quantity - (discountedQuantity * promotionQuantity)) * cartItemData.Product.Price;
-                //            cart.CartItems.First(c => c.Id == cartItemData.Id).IsPromotionApplied = true;
-                //            result.CartItems.Add(cartItemData);
-                //        }
-                //    }
-                //}
                 if (promotion.IsBundledPromotion == true)
                 {
                     var cartProducts = cart.CartItems.Where(c => promotion.Products.Any(p => p.ProductId == c.Product.Id && c.Quantity >= p.DiscountQuantity) && c.IsPromotionApplied == false);
@@ -147,19 +120,10 @@ namespace PromotionProcessingApp
                                 cartProduct.SubTotal = promotion.Value;
                             }
                             cartProduct.IsPromotionApplied = IsPromotionApplied = true;
-                            result.CartItems.Add(cartProduct);
+                            //result.CartItems.Add(cartProduct);
+                            result.CartItems.Where(c => c.Id == cartProduct.Id).Select(c => { return cartProduct; });
                         }
                     }
-                }
-            }
-
-            var nonDiscountedCartProducts = cart.CartItems.Where(c => c.IsPromotionApplied == false);
-            if (nonDiscountedCartProducts != null && nonDiscountedCartProducts.Any())
-            {
-                foreach (var nonDiscountedCartProduct in nonDiscountedCartProducts)
-                {
-                    nonDiscountedCartProduct.SubTotal = nonDiscountedCartProduct.Product.Price * nonDiscountedCartProduct.Quantity;
-                    result.CartItems.Add(nonDiscountedCartProduct);
                 }
             }
 
@@ -167,12 +131,57 @@ namespace PromotionProcessingApp
 
             return result;
         }
+    }
 
-        private static void AddSubTotal(Cart cart)
+    public class NonPromotionProducts : BasePromotion, IPromotionRule
+    {
+        public Cart CalculateCartTotal(Cart cart)
         {
-            if (cart != null && cart.CartItems.Any())
-                cart.Total = cart.CartItems.Sum(p => p.SubTotal);
+            Cart result = MapCart(cart);
+            var nonDiscountedCartProducts = cart.CartItems.Where(c => c.IsPromotionApplied == false);
+            if (nonDiscountedCartProducts != null && nonDiscountedCartProducts.Any())
+            {
+                foreach (var nonDiscountedCartProduct in nonDiscountedCartProducts)
+                {
+                    nonDiscountedCartProduct.SubTotal = nonDiscountedCartProduct.Product.Price * nonDiscountedCartProduct.Quantity;
+                    //result.CartItems.Add(nonDiscountedCartProduct);
+                    result.CartItems.Where(c => c.Id == nonDiscountedCartProduct.Id).Select(c => { return nonDiscountedCartProduct; });
+                }
+            }
+
+            AddSubTotal(result);
+            return result;
         }
+        
+    }
+
+    public class PromotionEngine
+    {
+        private IPromotionRepository promotionRepository;
+        private List<Promotion> _promotions = new List<Promotion>();
+
+        public PromotionEngine(IPromotionRepository promotionRepository)
+        {
+            this.promotionRepository = promotionRepository;
+            _promotions = promotionRepository.GetAllPromotions().ToList();
+        }
+
+        public Cart CalculateCartTotal(Cart cart)
+        {
+            if (cart != null && !cart.CartItems.Any())
+                return cart;
+
+            SingleProductFlatPricePromotion singleProductFlatPricePromotion = new SingleProductFlatPricePromotion();
+            BundledProductsFlatPricePromotion bundledProductsFlatPricePromotion = new BundledProductsFlatPricePromotion();
+            NonPromotionProducts nonPromotionProducts = new NonPromotionProducts();
+
+            _ = singleProductFlatPricePromotion.CalculateCartTotal(cart);
+            _ = bundledProductsFlatPricePromotion.CalculateCartTotal(cart);
+            var result = nonPromotionProducts.CalculateCartTotal(cart);
+
+            return result;
+        }
+
     }
 
 
